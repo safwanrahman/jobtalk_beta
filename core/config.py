ce2a7091484e080e872ca0196014842d4f3873d8
Chris Rodgers <xrodgers@gmail.com>
2017-06-06 14:54:50 -0400

    adding more choices to Positions (#5)
    * adding more choices to Positions
    
    * migration to change Positions choices

--- core/config.py
+++ core/config.py
@@ -1,10 +1,41 @@
+# These objects define the possible choices for multiple choice fields
+
 class Positions(object):
-    GRAD_STUDENT = 0
-    PROFESSOR = 1
+    """Choices for the `position` field in the `Review` model"""
+    ADMINISTRATOR = 0
+    LECTURER = 1
+    PHYSICIAN = 2
+    PROFESSOR_ADJUNCT = 3
+    PROFESSOR_ASSISTANT = 4
+    PROFESSOR_ASSOCIATE = 5
+    PROFESSOR_FULL = 6
+    PROFESSOR_EMERITUS = 7
+    POSTDOC = 8
+    RESEARCH_ASSOCIATE = 9
+    RESEARCH_SCIENTIST = 10
+    STUDENT_DOCTORAL = 11
+    STUDENT_MASTERS = 12
+    STUDENT_MEDICAL_DOCTOR = 13
+    TECHNICIAN = 14
+    OTHER = 15
 
     CHOICES = (
-        (GRAD_STUDENT, "Grad Student"),
-        (PROFESSOR, "Professor")
+        (ADMINISTRATOR, "Administrator"),
+        (LECTURER, "Lecturer"),
+        (PHYSICIAN, "Physician"),
+        (PROFESSOR_ADJUNCT, "Professor-Adjunct"),
+        (PROFESSOR_ASSISTANT, "Professor-Assistant"),
+        (PROFESSOR_ASSOCIATE, "Professor-Associate"),
+        (PROFESSOR_FULL, "Professor-Full"),
+        (PROFESSOR_EMERITUS, "Professor-Emeritus"),
+        (POSTDOC, "Postdoc"),
+        (RESEARCH_ASSOCIATE, "Research Associate"),
+        (RESEARCH_SCIENTIST, "Research Scientist"),
+        (STUDENT_DOCTORAL, "Student-Doctoral"),
+        (STUDENT_MASTERS, "Student-Master's"),
+        (STUDENT_MEDICAL_DOCTOR, "Student-Medical Doctor"),
+        (TECHNICIAN, "Technician"),
+        (OTHER, "Other"),
     )
 
 
