84687951278158345f9813d1e0ef70b61c51ffb8
Chris Rodgers <xrodgers@gmail.com>
2017-06-07 21:15:23 -0400

    review list view

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView
 
 from core.forms import UserRegistrationForm, ReviewForm
 from core.models import User, Review
@@ -21,6 +21,15 @@ class ReviewSubmission(CreateView):
     form_class = ReviewForm
     model = Review
     success_url = reverse_lazy('successful')
+
+
+class ReviewList(ListView):
+    model = Review
+    context_object_name = 'review_list'
+    
+
+class ReviewDetail(DetailView):
+    model = Review
 
 
 def successful(request):
