a477710c9b896c43315e61f6d936ec693cd8f00d
Chris Rodgers <xrodgers@gmail.com>
2017-06-14 16:54:45 -0400

    doc and verbose_name of fields

--- core/models.py
+++ core/models.py
@@ -10,6 +10,14 @@ from core.config import Positions, ResearchFields, Genders, Ethnicity, Departmen
 
 
 class User(AbstractUser):
+    """Model definition of User, a user of the site.
+    
+    These properties define the characteristics of a typical user of the
+    site, such as their current affiliation, interests, and identity.
+    
+    In the user registration form, users will see the `verbose_name`
+    text next to the field.
+    """
     institution = models.CharField(max_length=250, 
         verbose_name="What is your current institution?",
     )
@@ -42,26 +50,81 @@ class User(AbstractUser):
 
 
 class Review(models.Model):
-    institution = models.CharField(max_length=250)
-    department = models.IntegerField(choices=Departments.CHOICES)
-    position = models.IntegerField(choices=Positions.CHOICES)
-    year = models.IntegerField(verbose_name='What year did your position begin?')
-    experience = models.TextField(verbose_name='Describe your general experience')
-    gender = models.IntegerField(choices=Genders.CHOICES, verbose_name='How do you identify')
-    ethnicity = models.IntegerField(choices=Ethnicity.CHOICES)
-    supported = models.IntegerField(verbose_name='Do you feel supported?')
-    collaborative = models.NullBooleanField(blank=True)
-    tenue_rate = models.IntegerField(verbose_name='Estimate tenure rate for new professors')
-    gender_ratio = models.IntegerField(verbose_name='Estimate gender ratio (% women faculty)')
-    children = models.IntegerField(blank=True, null=True)
-    supported = models.IntegerField(verbose_name='Do you feel supported?')
-    hr_text = models.TextField(verbose_name='have you had to deal with HR and how did they handle it?')
-    satisfaction = models.IntegerField()
-    comment = models.TextField()
-    annual_salary = models.IntegerField(verbose_name='Annual salary (USD)')
+    """Model definition of Review, a review of an institution.
+    
+    These are submitted by Users. This encapsulates the opinions of a
+    User about a certain position at an institution. Some of the fields
+    may be auto-filled from the User's profile, if available.
+
+    In the review submission, users will see the `verbose_name`
+    text next to the field.
+    """
+    # Relationship of the User with the institution
+    # These may be auto-filled from User
+    institution = models.CharField(max_length=250,
+        verbose_name='What institution are you reviewing?',
+    )
+    department = models.IntegerField(choices=Departments.CHOICES,
+        verbose_name='Department',
+    )
+    position = models.IntegerField(choices=Positions.CHOICES,
+        verbose_name='What is (or was) your position at that institution?',
+    )
+    year = models.IntegerField(
+       verbose_name='What year did your position begin?',
+    )
+    experience = models.TextField(
+        verbose_name='Describe your general experience',
+    )
+    
+    # User's identity
+    gender = models.IntegerField(choices=Genders.CHOICES, 
+        verbose_name='Gender',
+    )
+    ethnicity = models.IntegerField(choices=Ethnicity.CHOICES,
+        verbose_name='Ethnicity',
+    )
+    
+    # Atmosphere
+    supported = models.IntegerField(
+        verbose_name='Do (or did) you feel supported?'
+    )
+    collaborative = models.NullBooleanField(blank=True,
+        verbose_name='Did you feel this is (or was) a collaborative position?',
+    )
+    tenue_rate = models.IntegerField(
+        verbose_name='Estimate tenure rate for new professors',
+    )
+    gender_ratio = models.IntegerField(
+        verbose_name='Estimate gender ratio (% women faculty)',
+    )
+    children = models.IntegerField(blank=True, null=True,
+        verbose_name='Did you have children (during your experience here)?'
+    )
+    supported = models.IntegerField(
+        verbose_name='Do you feel supported?'
+    )
+    hr_text = models.TextField(
+        verbose_name='Have you had to deal with HR and how did they handle it?',
+    )
+    satisfaction = models.IntegerField(
+        verbose_name='General satisfaction (1-10)'
+    )
+    comment = models.TextField(
+        verbose_name='Any additional comments?',
+    )
+    
+    # Compensation and benefits
+    annual_salary = models.IntegerField(
+        verbose_name='Annual salary (USD)'
+    )
     travel_allowance = models.IntegerField()
     pension = models.IntegerField()
     quality = models.IntegerField()
+    
+    # Workload
+    
+    # Training program (students and postdocs only)
 
     def get_absolute_url(self):
         return reverse('review-detail', args=[self.pk])
